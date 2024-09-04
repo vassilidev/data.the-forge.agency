67c0ccd184b6fda00b5ad751c8effee9a1abe29f
vassilidev <vassilidevnet@gmail.com>
2024-09-03 20:10:18 -0400

    feat: add default app service provider and telescope for dev

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,24 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        Schema::defaultStringLength(191);
+
+        if ($this->app->environment('production')) {
+            Model::shouldBeStrict();
+
+            URL::forceScheme('https');
+        }
     }
 }
